3b57bdec121787c3599d2924b1edc8a7a8ddca7c
webprodigies <webprodigies.connect@gmail.com>
2023-10-14 14:28:55 -0400

    making changes

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -9,22 +9,30 @@ import { DashboardSetup } from '@/components/dashboard-setup/dashboard-setup';
 import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
 
 const Dashboard = async () => {
+  // Initialize a Supabase client using server-side cookies
   const supabase = createServerComponentClient({ cookies });
+
+  // Fetch user data to determine if the user is authenticated
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
+  // If there's no authenticated user, exit the component
   if (!user) return;
 
+  // Query the database to find the workspace owned by the user
   const workspace = await db.query.workspaces.findFirst({
     where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
   });
 
+  // Retrieve the user's subscription status
   const { data: subscription, error: subscriptionError } =
     await getUserSubscriptionStatus(user.id);
 
+  // If there is an error in fetching the subscription status, exit the component
   if (subscriptionError) return;
 
+  // If the user doesn't have a workspace, render the DashboardSetup component
   if (!workspace)
     return (
       <div className="bg-background h-screen w-screen flex justify-center items-center">
@@ -35,6 +43,7 @@ const Dashboard = async () => {
       </div>
     );
 
+  // Redirect the user to their dashboard since they have a workspace
   redirect(`/dashboard/${workspace.id}`);
 };
 
